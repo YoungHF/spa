840b1aeb4994aa48dacbb5c57a81d20cbb6727ad
杨洪峰 <929662659@qq.com>
2018-05-27 20:59:34 +0800

    fix error & add unit & add karma.conf.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,27 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   env: {
     browser: true,
+    es6: true
   },
   extends: [
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    "standard"
   ],
   // add your custom rules here
   rules: {
     // allow async-await
-    'generator-star-spacing': 'off'
+    "generator-star-spacing": "off",
+    "indent": ["error",4],
+    "quotes": [ 2, "double" ],
+    "semi": ["error", "always"],
+    "no-unused-vars": ["off", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+    "no-new-func": "off",
+    "new-cap": "off",
+    "no-undef": "off",
+    "no-unused-expressions": "off"
   }
 }
