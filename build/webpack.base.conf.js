2c849168cf56d951a32c496368dd19d07f3e03e1
杨洪峰 <929662659@qq.com>
2018-05-27 23:48:02 +0800

    update webpack.base.conf.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -49,10 +49,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')],
-        options: {
-          plugins: ['istanbul']
-        }
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
